a32604065c975f85233bfedc9c6b1286f422d681
yesha.mavani <yesha.mavani@sourcefuse.com>
2023-03-31 11:13:17 +0530

    Initial commit

---
+++ packages/core/src/main/java/io/github/yeshamavani/core/Core.java
@@ -0,0 +1,9 @@
+package io.github.yeshamavani.core;
+
+public class Core {
+
+    public static String getHelloMessage() {
+        return "Helllloo World!!!!!!";
+    }
+}
+
---
+++ services/mainservice/src/main/java/io/github/yeshamavani/main/Main.java
@@ -0,0 +1,14 @@
+package io.github.yeshamavani.main;
+
+import io.github.yeshamavani.core.Core;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("In main");
+        String s = Core.getHelloMessage();
+        System.out.println(s);
+
+    }
+
+}
